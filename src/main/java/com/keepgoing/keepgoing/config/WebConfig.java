674954dfefc9847e283497ad5cb7164fecdc5502
Clsz <45088680+ClszK@users.noreply.github.com>
2025-11-19 16:02:21 +0900

    feat: #6 Swagger UI 연동 설정 및 API 문서 경로 변경

---
+++ src/main/java/com/keepgoing/keepgoing/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.keepgoing.keepgoing.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/api/**")
+                .addResourceLocations("file:./docs/api/");
+    }
+}
