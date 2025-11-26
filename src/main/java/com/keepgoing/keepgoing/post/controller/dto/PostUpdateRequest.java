76464e9a8b43f6a90ef801a27b8e2a1b7d5a693c
sumssumm <sumssum.p@gmail.com>
2025-11-26 17:16:26 +0900

    refactor: #10 Post 웹 DTO 패키지 및 정의 정리
    - post/dto 패키지를 controller/dto 하위로 이동
    - PostCreateRequest/PostUpdateRequest/PostResponse 패키지 변경
    - 컨트롤러 및 테스트에서 변경된 DTO 패키지 경로로 import 수정

---
+++ src/main/java/com/keepgoing/keepgoing/post/controller/dto/PostUpdateRequest.java
@@ -0,0 +1,26 @@
+package com.keepgoing.keepgoing.post.controller.dto;
+
+import com.keepgoing.keepgoing.post.domain.PostVisibility;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostUpdateRequest {
+
+    @NotBlank
+    @Size(max = 200)
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @NotNull
+    private PostVisibility visibility;
+
+    private boolean aiCollectable;
+}
