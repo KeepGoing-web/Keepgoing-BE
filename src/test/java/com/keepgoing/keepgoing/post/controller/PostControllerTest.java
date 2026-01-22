b7ba2b426a16e1d0865aabc3493ef746e9e5cef7
sumssumm <sumssum.p@gmail.com>
2026-01-22 18:28:07 +0900

    test: #17 PostControllerTest에서 Security 필터 비활성화

--- src/test/java/com/keepgoing/keepgoing/post/controller/PostControllerTest.java
+++ src/test/java/com/keepgoing/keepgoing/post/controller/PostControllerTest.java
@@ -14,6 +14,7 @@ import com.keepgoing.keepgoing.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.*;
@@ -31,6 +32,7 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(PostController.class)
 @Import(GlobalExceptionHandler.class)
 public class PostControllerTest {
