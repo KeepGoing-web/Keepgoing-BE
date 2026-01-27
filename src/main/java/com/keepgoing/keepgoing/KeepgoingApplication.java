670ff3508f41b0b652a916c3c8cb4859b76102ae
sumssumm <sumssum.p@gmail.com>
2026-01-27 16:47:53 +0900

    merge: sync develop into feat/21

--- src/main/java/com/keepgoing/keepgoing/KeepgoingApplication.java
+++ src/main/java/com/keepgoing/keepgoing/KeepgoingApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-<<<<<<< HEAD
-//@EnableJpaAuditing
-=======
->>>>>>> origin/develop
 @SpringBootApplication
 public class KeepgoingApplication {
 
