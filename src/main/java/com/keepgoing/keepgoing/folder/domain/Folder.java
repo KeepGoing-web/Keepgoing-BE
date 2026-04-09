90b072c23b764610af1fb41350ce78dc265a734a
sumssumm <sumssum.p@gmail.com>
2026-04-09 15:21:57 +0900

    refactor: #52-folder-move 폴더 이동 소유자 검증을 도메인에 반영

--- src/main/java/com/keepgoing/keepgoing/folder/domain/Folder.java
+++ src/main/java/com/keepgoing/keepgoing/folder/domain/Folder.java
@@ -76,8 +76,14 @@ public class Folder extends BaseEntity {
 		this.name = name;
 	}
 
-	public void moveTo(Folder parent) {
-		this.parent = parent;
+	public void moveTo(Folder targetParent) {
+		if (targetParent != null) {
+			if (this.owner == null || this.owner.getId() == null) {
+				throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+			}
+			targetParent.validateOwner(this.owner.getId());
+		}
+		this.parent = targetParent;
 	}
 
 	public void softDelete() {
